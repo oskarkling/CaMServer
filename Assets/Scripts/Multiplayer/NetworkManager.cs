3d5b76f0b995cdc9313674d3b368872b6ed10ad0
oskarkling <oskarkling@gmail.com>
2022-02-02 12:00:24 +0100

    added network code for player connection and disconnect

--- Assets/Scripts/Multiplayer/NetworkManager.cs
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -2,6 +2,16 @@ using RiptideNetworking;
 using RiptideNetworking.Utils;
 using UnityEngine;
 
+public enum ServerToClientId : ushort
+{
+    playerSpawned = 1,
+}
+
+public enum ClientToServerId : ushort
+{
+    name = 1,
+}
+
 public class NetworkManager : MonoBehaviour
 {
     private static NetworkManager _singleton;
@@ -35,10 +45,13 @@ public class NetworkManager : MonoBehaviour
 
     public void Start()
     {
+        Application.targetFrameRate = 60;
+        
         RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, false);
 
         Server = new Server();
         Server.Start(port, maxClientCount);
+        Server.ClientDisconnected += PlayerLeft;
     }
 
     private void FixedUpdate()
@@ -49,6 +62,11 @@ public class NetworkManager : MonoBehaviour
 
     private void OnApplicationQuit()
     {
-        Server.Stop();
+        Server.Stop(); 
+    }
+
+    private void PlayerLeft(object sender, ClientDisconnectedEventArgs e)
+    {
+        Destroy(Player.list[e.Id].gameObject);
     }
 }
